acfd00511c13fe2a2cc746162d0e2e25438ac032
nlweeks <noah.weeks@gmail.com>
2025-03-20 13:57:11 -0400

    Initial Commit

---
+++ Nudge/NudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeApp.swift
+//  Nudge
+//
+//  Created by Noah Weeks on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct NudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
